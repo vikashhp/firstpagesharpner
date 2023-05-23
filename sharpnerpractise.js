e4372fddb764a805103514133ff513a7754bcfd7
vikash <vickykumar89990@gmail.com>
2023-05-23 13:23:09 +0530

    added

---
+++ sharpnerpractise.js
@@ -0,0 +1,95 @@
+
+
+// console.log(document)
+// console.log(document.domain);
+// console.log(document.URL)
+// console.log(document.title)
+// // document.title=123
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.doctype)
+// console.log(document.forms)
+// console.log(document.link)
+// console.log(document.images)
+
+//Get element by id
+// console.log(document.getElementById('header-title'))
+
+// let head=document.getElementById('header-title');
+// head.innerText="Good bye"
+// head.textContent="Hello"
+// head.innerHTML='<h1>Chunnu</h1>'
+// head.style.borderBottom="solid 5px red"
+
+//Get element by className
+// let items=document.getElementsByClassName('list-group-item');
+
+// console.log(items)
+// items[1].textContent='hello';
+// items[1].style.fontWeight='bold';
+// // items[1].style.backgroundColor='red';
+
+// for(let i=0;i<items.length;i++){
+//     items[i].style.backgroundColor="#f4f4f4"
+// }
+
+
+//Get element by Tag Name 
+// let li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[2])
+// li[1].textContent='Hello'
+// li[2].textContent='Sun'
+// li[2].style.backgroundColor="red"
+// li[3].style.backgroundColor="yellow"
+
+// for(let i=0;i<li.length;i++)
+// {
+//     li[i].style.backgroundColor="#ccc"
+// }
+
+// queryselector
+
+// let header=document.querySelector('#main-header');
+
+// header.style.borderBottom='solid 5px red';
+
+
+// let input=document.querySelector('input');
+// input.value='Hello World'
+
+// let submit=document.querySelector('input[type="submit"]');
+// submit.value='SEND'
+
+// let item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// let lastitem=document.querySelector('.list-group-item:last-child');
+// lastitem.style.color="blue"
+
+// let seconditem=document.querySelector('.list-group-item:nth-child(2)');
+
+// seconditem.style.color="orange"
+
+// let thirditem=document.querySelector('.list-group-item:nth-child(3)');
+// thirditem.style.color="#f333";
+
+//Query Selector All
+
+// let title=document.querySelectorAll('.title');
+// // console.log(title)
+
+// // title[0].textContent="Hello"
+
+// let odd=document.querySelectorAll('li:nth-child(odd)')
+// let even=document.querySelectorAll('li:nth-child(even)')
+
+// for(let i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor="red";
+//     even[i].style.backgroundColor="orange"
+// }
+
+
+
+
