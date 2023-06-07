76a458d4dd1baa9453cfc7f27b7df54cfa487161
vikash <vickykumar89990@gmail.com>
2023-06-07 10:04:02 +0530

    added

--- crud.js
+++ crud.js
@@ -2,13 +2,14 @@
 let btn=document.querySelector('.form')
 
 btn.addEventListener('submit',storedata)
-let arr=JSON.parse(localStorage.getItem('data'))||[]
-let main_div=document.getElementById('display');
+// let arr=JSON.parse(localStorage.getItem('data'))||[]
+
 
 window.addEventListener('DOMContentLoaded',()=>{
 
-    axios.get("https://crudcrud.com/api/936b58827b324ec2bab30d628ba71273/studentdata")
+    axios.get("https://crudcrud.com/api/fbcd9888cf7041f8a56cfa8914db62ca/studentdata")
     .then((res)=>{
+        console.log(res.data)
         for(let i=0;i<res.data.length;i++){
             showinscreen(res.data[i])
         }
@@ -31,6 +32,9 @@ function storedata(e)
     let name=document.getElementById('name').value;
     let email=document.getElementById('email').value;
     let phone=document.getElementById('number').value;
+
+    // let deletebtn=document.createElement('button');
+    // deletebtn.innerText="Delete"
    
     
     
@@ -41,7 +45,7 @@ function storedata(e)
      
     // localStorage.setItem('data',JSON.stringify(arr))
 
-    axios.post("https://crudcrud.com/api/936b58827b324ec2bab30d628ba71273/studentdata",obj)
+    axios.post("https://crudcrud.com/api/fbcd9888cf7041f8a56cfa8914db62ca/studentdata",obj)
     .then((res)=>{
         console.log(res)
         showinscreen(res.data)
@@ -52,23 +56,52 @@ function storedata(e)
 
 }
 
+let deletebtn
+
+
 function showinscreen(data){
 
-    let name=data.name;
-    let email=data.email;
-    let phone=data.phone;
+    //   document.getElementById('email').value='';
+    //   document.getElementById('name').value='';
+    //   document.getElementById('phone').value='';
+     
+      const parent=document.getElementById('display')
+      const childhtml=`<li id=${data._id}>${data.name}-${data.email}-${data.phone}
+                       <button onclick=deleteUser('${data._id}')>Delete</button>
+      </li>`
 
-    let div=document.createElement('li');
-
-    
-    div.appendChild(document.createTextNode("name-->"+name+"  "));
-
-    div.appendChild(document.createTextNode("Email-->"+email+"  "));
-
-    div.appendChild(document.createTextNode("Phone-->"+phone+"  "));
-
-
-    main_div.appendChild(div)
+      parent.innerHTML=parent.innerHTML+childhtml;
 
 
 }
+
+
+
+function deleteUser(userId){
+    // console.log(userId)
+
+axios.delete(`https://crudcrud.com/api/fbcd9888cf7041f8a56cfa8914db62ca/studentdata/${userId}`)
+//   console.log(user)
+.then((res)=>{
+    removeuserfromscreen(userId)
+    console.log(userId)
+    // console.log(res)
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+// console.log(userId)
+// console.log('chunnu')
+}
+
+function removeuserfromscreen(userId){
+   const main_div=document.getElementById('display')
+    const child=document.getElementById(userId);
+    if(child){
+        main_div.removeChild(child)
+    }
+
+}
+
+
